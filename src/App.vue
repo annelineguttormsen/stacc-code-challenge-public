a6cb5985c2fe0d748c5b456d5e0243af83cd480a
Anneline Guttormsen <anneline.guttormsen@hotmail.no>
2022-06-30 19:27:49 +0200

    init commit: basic setup vue project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style lang="scss">
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+  html {
+    font-size: 16px;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Hvis det skulle være bruk for en side til, så har vi hvertfall router</h1>
+  </div>
+</template>
