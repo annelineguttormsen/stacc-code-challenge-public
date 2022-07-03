19bbba949d74ec6565b3058f3c95daff5fabdf10
Anneline Guttormsen <anneline.guttormsen@hotmail.no>
2022-07-04 01:24:40 +0200

    chore: added footer

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
   <main class="layout">
     <router-view/>
   </main>
+  <footer>
+    <ul>
+      <li>
+        <a href="https://www.opensanctions.org/docs/api/" target="_blank">Open Sanctions API</a>
+      </li>
+      <li>
+        <a href="https://github.com/annelineguttormsen/stacc-code-challenge-public" target="_blank">GitHub</a>
+      </li>
+    </ul>
+  </footer>
 </template>
 
 <style lang="scss">
@@ -11,5 +21,15 @@
     max-width: 1000px;
     margin: 0 auto;
     padding: 5rem 1rem 1rem;
+  }
+  footer {
+    ul {
+      list-style-type: none;
+      display: flex;
+      justify-content: center;
+      gap: 1rem;
+      flex-flow: row wrap;
+      padding: 0;
+    }
   }
 </style>
