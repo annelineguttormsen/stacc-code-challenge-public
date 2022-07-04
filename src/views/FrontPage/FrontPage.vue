777e0375adbf712f74984c7d727465177f72b938
Anneline Guttormsen <anneline.guttormsen@hotmail.no>
2022-07-04 02:04:45 +0200

    chore: moved search icon from input to button

--- src/views/FrontPage/FrontPage.vue
+++ src/views/FrontPage/FrontPage.vue
@@ -105,9 +105,13 @@
       appearance: none;
       line-height: 1.5;
       font-size: 1rem;
-      padding: 0.5rem 1rem;
+      padding: 0.5rem 1rem 0.5rem 2.25rem;
       transition: all .05s linear;
       background-color: var(--main-color);
+      background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' height='1em' width='1em' fill='%23fff'%3E%3Cpath fill='none' d='M0 0h24v24H0z'%3E%3C/path%3E%3Cpath d='M18.031 16.617l4.283 4.282-1.415 1.415-4.282-4.283A8.96 8.96 0 0 1 11 20c-4.968 0-9-4.032-9-9s4.032-9 9-9 9 4.032 9 9a8.96 8.96 0 0 1-1.969 5.617zm-2.006-.742A6.977 6.977 0 0 0 18 11c0-3.868-3.133-7-7-7-3.868 0-7 3.132-7 7 0 3.867 3.132 7 7 7a6.977 6.977 0 0 0 4.875-1.975l.15-.15z'%3E%3C/path%3E%3C/svg%3E");
+      background-size: 1.25rem;
+      background-repeat: no-repeat;
+      background-position: center left 0.5rem;
       color: #fff;
       font-weight: 600;
       user-select: none;
@@ -116,7 +120,7 @@
         background-color: var(--main-color-dark);
       }
       &:focus {
-        outline: 3px solid lightblue;
+        outline: 3px solid var(--blue-color);
       }
       &:active {
         background-color: var(--main-color-darkest);
